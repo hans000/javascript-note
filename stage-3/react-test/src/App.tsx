13d76ed25f983502e8e4f884d9021b6a2ccf6649
hans <haima_kc@163.com>
2020-06-20 11:09:13 +0800

    添加黑白无双项目

--- stage-3/react-test/src/App.tsx
+++ stage-3/react-test/src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Test001 from './001/index-solve'
 import BlackWhiteTurn from './黑白迭代';
+import BlackWhiteBoth from './黑白无双';
 import ForwardRef from './ref-forwardRef'
 import Children from './children'
 import Game2 from './game02'
+import Animate from './animate'
 
 function App() {
     return (
         <div className="App">
           {/* <Test001 /> */}
           {/* <BlackWhiteTurn /> */}
-          <ForwardRef />
+          {/* <ForwardRef /> */}
           {/* <Children /> */}
           {/* <Game2 /> */}
+          {/* <Animate /> */}
+          <BlackWhiteBoth />
         </div>
     );
 }
