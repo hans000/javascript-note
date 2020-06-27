2a03ad516183b7cc6c7002b16349dbacdc9921d7
hans <haima_kc@163.com>
2020-06-27 10:38:55 +0800

    音乐可视化

---
+++ stage-3/normal-test/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development', // development production
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+        globalObject: 'this',
+        publicPath: "",
+    },
+    devtool: 'eval‐source‐map',
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: 'ts-loader' },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8080,
+        publicPath: '/',
+    }
+}
