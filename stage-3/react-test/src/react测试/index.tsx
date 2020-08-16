7f98e5114dbe1745c08172364b95b533766811f7
hans <haima_kc@163.com>
2020-08-16 18:18:43 +0800

    测试图片预览

--- stage-3/react-test/src/react测试/index.tsx
+++ stage-3/react-test/src/react测试/index.tsx
@@ -3,11 +3,12 @@ import ComplexObject from './复杂对象更新问题'
 import Repeat from './路由跳转重复渲染问题'
 import TextA from './TextArea'
 import AAA from './移动端下载附件.test'
+import File from './react附件预览'
 
 export default function() {
     return (
         <div>
-            <AAA />
+            <File />
         </div>
     )
 }
