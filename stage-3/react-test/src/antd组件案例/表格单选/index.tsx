ac69a0b6d645cd11e1b62b91b6d031145643cfad
hans <haima_kc@163.com>
2020-08-30 20:23:21 +0800

    feat: antd 表格单选的实现方式

--- stage-3/react-test/src/antd组件案例/表格单选/index.tsx
+++ stage-3/react-test/src/antd组件案例/表格单选/index.tsx
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { Table, Button } from 'antd'
+import './index.css' // 通过覆盖样式的方式吧多选控件隐藏
 
-export default class index extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+const columns = [
+    { dataIndex: 'name', title: '名字' },
+    { dataIndex: 'number', title: '电话' },
+]
+
+const data = [
+    { name: 'zhang san', number: '12456' },
+    { name: 'li si', number: '9875' },
+    { name: 'wang wu', number: '2432543' },
+    { name: 'zhao liu', number: '3444656' },
+]
+
+export default function() {
+    const [selectedRowKeys, setSelectedRowKeys] = useState([])
+    const [selectedRows, setSelectedRows] = useState([])
+    
+    const onChange = (keys: string[] | number[], rows: any[]) => {
+        // ! 核心代码
+        // 更新数据前判断勾选为2时把前一次的勾选从数组中移除
+        if (keys.length === 2) {
+            const index = selectedRowKeys[0] === keys[0] ? 0 : 1
+            rows.splice(index, 1)
+            keys.splice(index, 1)
+        }
+        setSelectedRowKeys(() => keys)
+        setSelectedRows(() => rows)
     }
+
+    const clickHandle = () => {
+        console.log(selectedRowKeys, selectedRows);
+    }
+
+    return (
+        <div>
+            <div>
+                <Button onClick={clickHandle}>click me</Button>
+            </div>
+            <Table
+                className='single-table'
+                columns={columns}
+                dataSource={data}
+                rowSelection={{
+                    selectedRowKeys,
+                    onChange,
+                }}
+                rowKey='name' />
+        </div>
+    )
 }
