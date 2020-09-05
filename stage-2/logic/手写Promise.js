3ada5594698ae069690064a77626e19b1933b0b6
hans <haima_kc@163.com>
2020-09-05 20:55:41 +0800

    fix 完善promise

--- stage-2/logic/手写Promise.js
+++ stage-2/logic/手写Promise.js
@@ -1,26 +1,29 @@
-const PENDING = 'PENDING'
-const RESOLVED = 'RESOLVED'
-const REJECTED = 'REJECTED'
+const PENDIGN = 'pending'
+const RESOLVED = 'resolved'
+const REJECTED = 'rejected'
 
 class MyPromise {
 	constructor(executor) {
-		this.status = PENDING
-		this.value = undefined
-		this.reason = undefined
-		this.onResolvedCallbacks = []
-		this.onRejectedCallbacks = []
-		let resolve = (value) => {
-			if (this.status === PENDING) {
-				this.value = value
+		this.status = PENDIGN
+		this.data = undefined
+		this.callbacks = []
+		
+		const resolve = (value) => {
+			if (this.status === PENDIGN) {
+				this.data = value
 				this.status = RESOLVED
-				this.onResolvedCallbacks.forEach(fn => fn())
+				setTimeout(() => {
+					this.callbacks.forEach(({ onfulfilled }) => onfulfilled())
+				})
 			}
 		}
-		let reject = (reason) => {
-			if (this.status === PENDING) {
-				this.reason = reason
+		const reject = (reason) => {
+			if (this.status === PENDIGN) {
+				this.data = reason
 				this.status = REJECTED
-				this.onRejectedCallbacks.forEach(fn => fn())
+				setTimeout(() => {
+					this.callbacks.forEach(({ onrejected }) => onrejected())
+				})
 			}
 		}
 		try {
@@ -30,50 +33,70 @@ class MyPromise {
 		}
 	}
 	catch(onrejected) {
-		if (this.status === REJECTED) {
-			onrejected(this.reason)
-			return this
-		}
-		if (this.status === PENDING) {
-			this.onRejectedCallbacks.push(() => {
-				onrejected(this.reason)
-			})
-			return this
-        }
+		return this.then(undefined, onrejected)
 	}
 	then(onfulfilled, onrejected) {
-		if (this.status === RESOLVED) {
-			onfulfilled(this.value)
-			return this
-		}
-		if (this.status === REJECTED) {
-			onrejected(this.reason)
-			return this
-		}
-		if (this.status === PENDING) {
-			this.onResolvedCallbacks.push(() => {
-				onfulfilled(this.value)
-			})
-			this.onRejectedCallbacks.push(() => {
-				onrejected(this.reason)
-			})
-			return this
-        }
+		onfulfilled = typeof onfulfilled === 'function' ? onfulfilled : () => this.data
+		onrejected = typeof onrejected === 'function' ? onrejected : () => { throw this.data }
+
+		return new MyPromise((resolve, reject) => {
+			const handle = (callback) => {
+				try {
+					const result = callback(this.data)
+					result instanceof MyPromise ? result.then(resolve, reject) : resolve(result)
+				} catch (error) {
+					reject(error)
+				}
+			}
+
+			if (this.status === PENDIGN) {
+				this.callbacks.push({
+					onfulfilled: () => handle(onfulfilled),
+					onrejected: () => handle(onrejected),
+				})
+			} else {
+				setTimeout(() => {
+					this.status === RESOLVED ? handle(onfulfilled) : handle(onrejected)
+				})
+			}
+		})
 	}
 }
+MyPromise.resolved = (value) => {
+	return new MyPromise((resolved) => {
+		resolved(value)
+	})
+}
+MyPromise.rejected = (reason) => {
+	return new MyPromise((_, rejected) => {
+		rejected(reason)
+	})
+}
 
-const promise = new MyPromise((resolve) => {
-    setTimeout(() => {
-        resolve(12)
-    }, 1000)
+const p1 = () => new MyPromise((resolve) => {
+	console.log(1);
+	// let p2 = new MyPromise((resolve) => {
+	// 	console.log(2);
+	// 	setTimeout(() => {	// 宏task 1
+	// 		console.log(3);
+	// 	}, 0)
+	// 	resolve(5);
+	// });
+	resolve(6);
+	// p2.then((arg) => {
+	// 	console.log(arg);
+	// });
 })
-console.log('start')
-promise.then(value => {
-    console.log(value)
-})
-promise.then(value => {
-    console.log(value)
-})
-MyPromise.resolve(123)
-
-Promise.resolve()
+setTimeout(() => {	// 宏task 1
+	console.log(8);
+	// new MyPromise(resolve => {
+	// 	resolve(9);
+	// }).then(res => {
+	// 	console.log(res)
+	// })
+}, 0)
+p1().then((arg) => {
+	console.log(arg);
+});
+console.log(10);
+// 1 2 10 5 6 8 9 3
